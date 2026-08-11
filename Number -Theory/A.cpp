9c7352ebfd477d218f27865bf64317c6220dc716
Sayem Islam <sayem.shuvo999@gmail.com>
2026-08-11 08:17:40 +0600

    check

---
+++ Number -Theory/A.cpp
@@ -0,0 +1,33 @@
+//Author : Sayem Islam
+// Generated on 2026-08-11 08:16:25
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define MOD 1000000007
+
+void solve()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tc = 1;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
